134a4d16439599bfd77a0c6ce363169a8a9e9b5d
harmstr2 <harmstrong@officeworks.com.au>
2016-03-29 17:55:01 +1100

    Updated to at least basic example

--- tasks/force.js
+++ tasks/force.js
@@ -1,35 +1,43 @@
 'use strict'
 
-/**
- * TODO: Create separate project for the actual main functionality so that
- * it can be made into command line or otherwise
- */
-var Force = require('../lib/force');
+var Force = require('sf-jedi');
 
 /**
  * Module export for the grunt task setup
- *
  * @param grunt - This is grunt as provided by the load[Npm]Tasks function
  */
 module.exports = function(grunt) {
   grunt.registerMultiTask('force', 'A suite of grunt tasks for salesforce development', function() {
-
     // Set the task defaults here
     let defaults = {
-      useGit: false,
-      username: process.env.SF_USER,
+      username: process.env.SF_USERNAME,
       password: process.env.SF_PASSWORD,
       token: process.env.SF_TOKEN,
-      host: process.env.SF_HOST
+      host: process.env.SF_HOST,
+      apiVersion: 34.0,
+      logLevel: 'info'
     }
 
     // Mixin any provided options
     defaults = this.options(defaults);
 
     // Create the force object
-    let force = new Force(defaults);
+    try {
+      let force = new Force(defaults);
 
-    // Call the relevant method
-    force[this.target]();
+
+      // All the force tasks are async
+      let done = this.async();
+
+      // Call the relevant method
+      force[this.target]()
+        .then(result => done())
+        .catch(err => {
+          grunt.log.writeln(err);
+          done();
+        });
+    } catch (e) {
+      grunt.log.error(e);
+    }
   });
 };
